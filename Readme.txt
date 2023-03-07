a0fcf0717d1020b1f81f532cd79df138f4c622cf
Varun Sharma <varunsh170502@gmail.com>
2023-03-07 14:19:08 +0530

    Completed

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+HOME 
+    ABOUT
+    PRODUCTS
+    CONTACT
+
+# Each Page has separate CSS file linked. 
+# Main.js is the js file for every script/function call.
+# SMTP.JS is used for contact us page to mail.
+# For product us page, filter is done using Javascript whose function is written in Main.js
+# Bootstrap used is @5 and sometimes @4
--- assets/js/main.js
+++ assets/js/main.js
@@ -321,22 +321,3 @@ document.addEventListener('DOMContentLoaded', () => {
 })();
 
 
-(function () {
-  const searchBox = document.querySelector('#search-item')
-  const storeItems = document.querySelectorAll('.store-item')
-
-  searchBox.addEventListener('keyup', (e) => {
-
-    const searchFilter = e.target.value.toLowerCase().trim()
-
-
-    storeItems.forEach((item) => {
-
-      if (item.textContent.includes(searchFilter)) {
-        item.style.display = 'block'
-      } else {
-        item.style.display = 'none'
-      }
-    })
-  })
-})();
